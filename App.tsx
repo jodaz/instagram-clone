f104be64546e5b52d049e6d8a2ea99fee497aa5f
Jesus Ordosgoitty <jesuodz@gmail.com>
2023-04-29 17:02:12 -0400

    practicing state and functions

--- App.tsx
+++ App.tsx
@@ -1,13 +1,45 @@
+import * as React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hola mundo</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [state, setState] = React.useState(0)
+
+    const add  = () => {
+        setState(state + 1)
+    }
+
+    const substract  = () => {
+        setState(state - 1)
+    }
+
+    const reset  = () => {
+        setState(0)
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={{ color: 'black' }}>Hola mundo</Text>
+            <Text style={{ color: 'black' }}>{state}</Text>
+
+            <StatusBar style="auto" />
+            <TouchableOpacity
+                onPress={() => add()}
+            >
+                <Text style={{ fontSize: 25 }}>Add</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => substract()}
+            >
+                <Text style={{ fontSize: 25 }}>Substract </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => reset()}
+            >
+                <Text style={{ fontSize: 25 }}>reset </Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
